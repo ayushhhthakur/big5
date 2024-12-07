53e8a4a8065a5f49809401ed0ff798667dbaad11
Ayush Thakur <ayushthakur1412@gmail.com>
2024-12-07 14:18:26 +0530

    fixed css embedd

--- src/embed.tsx
+++ src/embed.tsx
@@ -2,8 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { EmbedTest } from './components/EmbedTest'
 import './index.css'
+import './embed.css'
 
-ReactDOM.createRoot(document.getElementById('big-five-embed')!).render(
+// Get theme from URL parameters
+const urlParams = new URLSearchParams(window.location.search);
+const theme = urlParams.get('theme') || 'light';
+
+// Create container if it doesn't exist
+let container = document.getElementById('big-five-embed');
+if (!container) {
+  container = document.createElement('div');
+  container.id = 'big-five-embed';
+  document.body.appendChild(container);
+}
+
+// Set theme
+container.setAttribute('data-theme', theme);
+
+ReactDOM.createRoot(container).render(
   <React.StrictMode>
     <EmbedTest />
   </React.StrictMode>,
